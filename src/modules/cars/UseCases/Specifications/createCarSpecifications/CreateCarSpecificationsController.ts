50c9f9b45ef576a40f6f88dfe352cb8b1c83dce3
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-03-23 19:11:10 -0300

    refactor: finished createSpecificationsController

--- src/modules/cars/UseCases/Specifications/createCarSpecifications/CreateCarSpecificationsController.ts
+++ src/modules/cars/UseCases/Specifications/createCarSpecifications/CreateCarSpecificationsController.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateCarSpecificationsUseCase } from './CreateCarSpecificationsUseCase';
 
 class CreateCarSpecificationsController {
-	async handle(request: Request, response: Response) {}
+	async handle(request: Request, response: Response): Promise<Response> {
+		const { id } = request.params;
+		const { specification_id } = request.body;
+
+		const createCarSpecificationsUseCase = container.resolve(CreateCarSpecificationsUseCase);
+
+		const cars = await createCarSpecificationsUseCase.execute({ car_id: id, specification_id });
+
+		return response.json(cars);
+	}
 }
 
 export { CreateCarSpecificationsController };
