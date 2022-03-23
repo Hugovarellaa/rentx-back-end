aaa81af7168b0d72a6431243261381f813cdf75e
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-03-23 19:12:04 -0300

    refactor: create route car-specification

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/UseCases/Car/createCar/CraeteCarController';
 import { ListAvailableCarsController } from '@modules/cars/UseCases/Car/listAvailableCar/ListAvailableCarsController';
+import { CreateCarSpecificationsController } from '@modules/cars/UseCases/Specifications/createCarSpecifications/CreateCarSpecificationsController';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
@@ -10,8 +11,10 @@ const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationsController = new CreateCarSpecificationsController();
 
 carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
 carsRoutes.get('/available', listAvailableCarsController.handle);
+carsRoutes.post('/specifications/:id', ensureAuthenticated, ensureAdmin, createCarSpecificationsController.handle);
 
 export { carsRoutes };
