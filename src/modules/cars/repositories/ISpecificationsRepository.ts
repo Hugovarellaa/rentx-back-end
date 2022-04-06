addf934f737dfd1bbccb0e3525e09a8b09a68a91
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-06 18:49:56 -0300

    refactor: return specification

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -2,7 +2,7 @@ import { ICreateSpecificationsDTO } from '../dtos/ICreateSpecificationsDTO';
 import { Specification } from '../infra/typeorm/entities/Specification';
 
 interface ISpecificationsRepository {
-	create({ name, description }: ICreateSpecificationsDTO): Promise<void>;
+	create({ name, description }: ICreateSpecificationsDTO): Promise<Specification>;
 	getAll(): Promise<Specification[]>;
 	findByName(name: string): Promise<Specification>;
 	findByIds(ids: string[]): Promise<Specification[]>;
