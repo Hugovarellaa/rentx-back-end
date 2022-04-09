a1e955c6b65070428e8928c1c701130a46edb3cd
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-09 18:00:42 -0300

    fix: SpecificationsInMemory - findByIds

--- src/modules/cars/UseCases/Specifications/createCarSpecifications/CreateCarSpecificationsUseCase.ts
+++ src/modules/cars/UseCases/Specifications/createCarSpecifications/CreateCarSpecificationsUseCase.ts
@@ -1,4 +1,5 @@
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IRequest {
@@ -11,6 +12,8 @@ class CreateCarSpecificationsUseCase {
 	constructor(
 		// @inject()
 		private carsRepository: ICarsRepository,
+		// @inject()
+		private specificationsRepository: ISpecificationsRepository,
 	) {}
 	async execute({ car_id, specification_id }: IRequest): Promise<void> {
 		const carExists = await this.carsRepository.findById(car_id);
@@ -18,6 +21,8 @@ class CreateCarSpecificationsUseCase {
 		if (!carExists) {
 			throw new AppError(`Car not found!`);
 		}
+
+		const specifications = await this.specificationsRepository.findByIds(specification_id);
 	}
 }
 
