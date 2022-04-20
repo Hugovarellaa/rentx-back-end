e36a36701334cc7b6c2749eae9c48a133dbc7fc6
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-20 17:34:38 -0300

    refactor: update

--- src/modules/cars/UseCases/Specifications/createCarSpecifications/CreateCarSpecificationsUseCase.spec.ts
+++ src/modules/cars/UseCases/Specifications/createCarSpecifications/CreateCarSpecificationsUseCase.spec.ts
@@ -1,10 +1,14 @@
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+
 import { CreateCarSpecificationsUseCase } from './CreateCarSpecificationsUseCase';
 
+let carsRepositoryInMemory: CarsRepositoryInMemory;
 let createCarSpecificationsUseCase: CreateCarSpecificationsUseCase;
 
 describe('Create Car specification ', () => {
 	beforeEach(() => {
-		createCarSpecificationsUseCase = new CreateCarSpecificationsUseCase();
+		carsRepositoryInMemory = new CarsRepositoryInMemory();
+		createCarSpecificationsUseCase = new CreateCarSpecificationsUseCase(carsRepositoryInMemory);
 	});
 
 	it('should be able to add a new specification to the car', () => {});
