a629a8c3d970df3a082adb66d69e84df4b5cded7
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-20 17:25:22 -0300

    fix: Create car specifications - test

--- src/modules/cars/UseCases/Specifications/createCarSpecifications/CreateCarSpecificationsUseCase.spec.ts
+++ src/modules/cars/UseCases/Specifications/createCarSpecifications/CreateCarSpecificationsUseCase.spec.ts
@@ -1,1 +1,11 @@
-describe('Create Car specification ', () => {});
+import { CreateCarSpecificationsUseCase } from './CreateCarSpecificationsUseCase';
+
+let createCarSpecificationsUseCase: CreateCarSpecificationsUseCase;
+
+describe('Create Car specification ', () => {
+	beforeEach(() => {
+		createCarSpecificationsUseCase = new CreateCarSpecificationsUseCase();
+	});
+
+	it('should be able to add a new specification to the car', () => {});
+});
