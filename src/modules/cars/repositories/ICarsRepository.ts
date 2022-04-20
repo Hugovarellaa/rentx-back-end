6c88d75d27656f1da5570eaaf838509db0017b28
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-20 17:34:00 -0300

    fix: Create findById

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -5,6 +5,7 @@ interface ICarsRepository {
 	create(data: ICreateCarsDTO): Promise<Car>;
 	findByLicensesPlate(license_plate: string): Promise<Car>;
 	findAllAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]>;
+	findById(id: string): Promise<Car>;
 }
 
 export { ICarsRepository };
