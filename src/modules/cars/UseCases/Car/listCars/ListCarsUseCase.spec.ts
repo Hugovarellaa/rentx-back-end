6cdc8f8c834047a3f37ccebec7785967fef6d7e3
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-13 14:27:45 -0300

    fix: should be able to list all available cars

--- src/modules/cars/UseCases/Car/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/UseCases/Car/listCars/ListCarsUseCase.spec.ts
@@ -1,1 +1,13 @@
-describe('List Cars', () => {});
+import { ListCarsUseCase } from './ListCarsUseCase';
+
+let listCarsUseCase: ListCarsUseCase;
+
+describe('List Cars', () => {
+	beforeEach(() => {
+		listCarsUseCase = new ListCarsUseCase();
+	});
+
+	it('should be able to list all available cars', async () => {
+		await listCarsUseCase.execute();
+	});
+});
