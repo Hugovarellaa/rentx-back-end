d428287413a7bf930c026c462de1cfa8a7a54df5
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-13 14:31:26 -0300

    fix: create method findAllAvailable

--- src/modules/cars/UseCases/Car/listCars/ListCarsUseCase.ts
+++ src/modules/cars/UseCases/Car/listCars/ListCarsUseCase.ts
@@ -1,9 +1,11 @@
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 
 class ListCarsUseCase {
 	constructor(private carsRepository: ICarsRepository) {}
-	async execute(): Promise<void> {
+	async execute(): Promise<Car[]> {
 		const cars = await this.carsRepository.findAllAvailable();
+		return cars;
 	}
 }
 
