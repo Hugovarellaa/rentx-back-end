6d8cc5a4dc8343ecf34e9fa139a10349349cf084
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-13 14:30:24 -0300

    fix: create method findAllAvailable

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -4,6 +4,7 @@ import { Car } from '../infra/typeorm/entities/Car';
 interface ICarsRepository {
 	create(data: ICreateCarsDTO): Promise<Car>;
 	findByLicensesPlate(license_plate: string): Promise<Car>;
+	findAllAvailable(): Promise<Car[]>;
 }
 
 export { ICarsRepository };
