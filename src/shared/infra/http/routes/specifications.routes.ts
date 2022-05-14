ee93a16322c8e67896f561363b86605c5566744e
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-14 14:15:49 -0300

    refactor: create specifications - Admin

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { CreateSpecificationsController } from '@modules/cars/UseCases/Specifications/createSpecifications/CreateSpecificationsController';
 import { ListSpecificationsController } from '@modules/cars/UseCases/Specifications/listSpecifications/ListSpecificationsController';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const specificationsRoutes = Router();
@@ -10,10 +11,7 @@ const specificationsRoutes = Router();
 const listSpecificationsController = new ListSpecificationsController();
 const createSpecificationsController = new CreateSpecificationsController();
 
-specificationsRoutes.use(ensureAuthenticated);
-
-specificationsRoutes.post('/', createSpecificationsController.handle);
-
+specificationsRoutes.post('/', ensureAuthenticated, ensureAdmin, createSpecificationsController.handle);
 specificationsRoutes.get('/', listSpecificationsController.handle);
 
 export { specificationsRoutes };
