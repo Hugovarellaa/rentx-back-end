024a0b0a1e603b3fbffee428f5ce65481698c2d3
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-29 13:57:05 -0300

    refactor: function ensure admin

--- src/shared/infra/http/middlewares/ensureAdmin.ts
+++ src/shared/infra/http/middlewares/ensureAdmin.ts
@@ -1,1 +1,3 @@
-export async function ensureAdmin() {}
+import { NextFunction, Request, Response } from 'express';
+
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction) {}
