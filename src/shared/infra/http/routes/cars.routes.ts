5ee1574684b09873a97bdc37c47f0456a4d58768
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-29 14:00:03 -0300

    fix: ensureAuthenticated in route cars

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/UseCases/Car/createCar/CraeteCarController';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 
-carsRoutes.post('/', createCarController.handle);
+carsRoutes.post('/', ensureAuthenticated, createCarController.handle);
 
 export { carsRoutes };
