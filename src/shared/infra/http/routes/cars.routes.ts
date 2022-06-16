a59f419bc906d9c78033f9a197bdc2b378b74438
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-06-16 11:17:53 -0300

    refactor: create method POST

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import { CreateCarController } from '@modules/cars/UseCases/Car/createCar/CraeteCarController';
+
 const carsRoutes = Router();
+
+const createCarController = new CreateCarController();
+
+carsRoutes.post('/', createCarController.handle);
 
 export { carsRoutes };
