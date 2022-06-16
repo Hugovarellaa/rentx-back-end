7951f1d8bdecd7b64507d550a85fffd6d03784e7
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-06-16 11:16:54 -0300

    refactor: import route carsRoutes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { authenticatesRoutes } from './autenticates.routes';
+import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
@@ -12,5 +13,6 @@ router.use(authenticatesRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 router.use('/users', usersRoutes);
+router.use('/cars', carsRoutes);
 
 export { router };
