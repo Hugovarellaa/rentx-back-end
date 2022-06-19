2962eccecfb790c277ef72008f9bf83fbbfc0c56
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-06-19 10:57:54 -0300

    fix: CarsRepository - method findByLicensesPlate

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -1,14 +1,22 @@
+import { getRepository, Repository } from 'typeorm';
+
 import { ICreateCarsDTO } from '@modules/cars/dtos/ICreateCarsDTO';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 
 import { Car } from '../entities/Car';
 
 class CarsRepository implements ICarsRepository {
+	private repository: Repository<Car>;
+
+	constructor() {
+		this.repository = getRepository(Car);
+	}
+
 	create(data: ICreateCarsDTO): Promise<Car> {
 		throw new Error('Method not implemented.');
 	}
-	findByLicensesPlate(license_plate: string): Promise<Car> {
-		throw new Error('Method not implemented.');
+	async findByLicensesPlate(license_plate: string): Promise<Car> {
+		return this.repository.findOne({ license_plate });
 	}
 }
 
