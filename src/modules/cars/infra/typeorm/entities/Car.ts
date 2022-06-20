70c94bc15aead6fde2325347ce42c49e98e3fc33
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-06-20 10:46:47 -0300

    refactor: Update - Connect Database typeorm in entities Cars

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,10 +1,12 @@
-import { Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
 @Entity('cars')
 class Car {
 	@PrimaryColumn()
 	id?: string;
+
+	@Column()
 	name: string;
 	description: string;
 	daily_rate: number;
