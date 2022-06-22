3e819dd9a267c460b7deb9259c2b88e48a356723
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-06-22 10:25:26 -0300

    fix: should be able to create a car with exists license plate

--- src/modules/cars/UseCases/Car/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/UseCases/Car/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 
 import { CreateCarUseCase } from './CreateCarUseCase';
 
@@ -23,5 +24,27 @@ describe('Create Car', () => {
 		});
 	});
 
-	it('should be able to create a car with exists license plate', () => {});
+	it('should be able to create a car with exists license plate', async () => {
+		expect(async () => {
+			await createCarUseCase.execute({
+				name: 'Name1',
+				description: 'Description Car',
+				daily_rato: 100,
+				license_plate: 'ABC-123',
+				fine_amount: 22,
+				brand: 'Brand',
+				category_id: 'category',
+			});
+
+			await createCarUseCase.execute({
+				name: 'Name2',
+				description: 'Description Car',
+				daily_rato: 100,
+				license_plate: 'ABC-123',
+				fine_amount: 22,
+				brand: 'Brand',
+				category_id: 'category',
+			});
+		}).rejects.toBeInstanceOf(AppError);
+	});
 });
