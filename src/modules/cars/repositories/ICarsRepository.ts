883c513d29431fa9bfe509b840c0d6340ef03ec7
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-06-22 10:27:25 -0300

    refactor: return car in interface create()

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -2,7 +2,7 @@ import { ICreateCarsDTO } from '../dtos/ICreateCarsDTO';
 import { Car } from '../infra/typeorm/entities/Car';
 
 interface ICarsRepository {
-	create(data: ICreateCarsDTO): Promise<void>;
+	create(data: ICreateCarsDTO): Promise<Car>;
 	findByLicensesPlate(license_plate: string): Promise<Car>;
 }
 
