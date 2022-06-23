6172d9376e817111a6d0c45cd51f0e55eb857ceb
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-06-23 00:15:58 -0300

    fix: Create repository in memory cars

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -1,10 +1,18 @@
 import { ICreateCarsDTO } from '@modules/cars/dtos/ICreateCarsDTO';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 
 import { ICarsRepository } from '../ICarsRepository';
 
 class CarsRepositoryInMemory implements ICarsRepository {
-	create(data: ICreateCarsDTO): Promise<void> {
-		throw new Error('Method not implemented.');
+	cars: Car[] = [];
+
+	async create(data: ICreateCarsDTO): Promise<void> {
+		const { brand, category_id, daily_rato, description, fine_amount, license_plate, name } = data;
+
+		const car = new Car();
+		Object.assign(car, { brand, category_id, daily_rato, description, fine_amount, license_plate, name });
+
+		this.cars.push(car);
 	}
 }
 
