c05cb356cadcfa7ee737b436d2ebd063112a849a
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-06-24 23:50:04 -0300

    fix: Create useCase cars

--- src/modules/cars/UseCases/Car/createCar/CreateCarUseCase.ts
+++ src/modules/cars/UseCases/Car/createCar/CreateCarUseCase.ts
@@ -1,6 +1,21 @@
+import { injectable } from 'tsyringe';
+
+interface IRequest {
+	name: string;
+	description: string;
+	daily_rato: number;
+	license_plate: string;
+	fine_amount: number;
+	brand: string;
+	category_id: string;
+}
+
+@injectable()
 class CreateCarUseCase {
 	constructor() {}
-	execute() {}
+	async execute(data: IRequest) {
+		const { name, description, daily_rato, license_plate, fine_amount, brand, category_id } = data;
+	}
 }
 
 export { CreateCarUseCase };
