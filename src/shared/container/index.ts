cb695c90bc565ae3b48956aef9974c3f22681e3e
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-07-04 20:25:12 -0300

    refactor: import

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,6 @@
 import { container } from 'tsyringe';
 
+import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 
 container.registerSingleton<ICategoriesRepository>('CategoriesRepository', CategoriesRepository);
