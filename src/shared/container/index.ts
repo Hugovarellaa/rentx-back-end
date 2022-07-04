a4c32d652b554293ac1c4f168dbf1594803c272f
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-07-04 20:25:25 -0300

    refactor: import

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,7 @@
 import { container } from 'tsyringe';
 
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
+import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 
