64df59d7688ff2f4d361d5bfc068d3498114419d
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-07-06 20:09:08 -0300

    refactor: imports

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 
+import uploadConfig from '@config/upload';
 import { CreateUsersController } from '@modules/accounts/useCases/createUsers/CreateUsersController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
