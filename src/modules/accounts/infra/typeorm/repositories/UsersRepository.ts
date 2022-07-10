83fbccf4cf4844572c234070b5d4863fa008a3b1
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-07-10 20:00:45 -0300

    refactor: imports

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -3,6 +3,8 @@ import { getRepository, Repository } from 'typeorm';
 import { ICreateUsersDTO } from '@modules/accounts/dtos/CreateUsersDTO';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 
+import { User } from '../entities/User';
+
 class UsersRepository implements IUsersRepository {
 	private repository: Repository<User>;
 
