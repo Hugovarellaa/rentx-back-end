a6bef32aa931c8b1aa1296338b5ae68f4f68a34e
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-07-15 19:34:22 -0300

    refactor: imports

--- src/modules/cars/UseCases/Categories/createCategories/CreateCategoriesUseCase.ts
+++ src/modules/cars/UseCases/Categories/createCategories/CreateCategoriesUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '@errors/AppError';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 
 interface IRequest {
