320d606d142c06c9e531a53173470277d8ac8cbb
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-07-15 19:34:42 -0300

    refactor: imports

--- src/modules/cars/UseCases/Categories/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/UseCases/Categories/importCategories/ImportCategoriesUseCase.ts
@@ -2,6 +2,8 @@ import { parse } from 'csv-parse';
 import fs from 'fs';
 import { inject, injectable } from 'tsyringe';
 
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
+
 interface IImportCategories {
 	name: string;
 	description: string;
