aef63766f93b13338fe942f2cd9e48d17efc3e05
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-01 17:23:56 -0300

    fix: Create test authenticated user

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,1 +1,16 @@
-describe('Authenticated user', () => {});
+import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory';
+import { CreateUsersUseCase } from '../createUsers/CreateUsersUseCase';
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let createUsersUseCase: CreateUsersUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+
+describe('Authenticated user', () => {
+	beforeEach(() => {
+		usersRepositoryInMemory = new UsersRepositoryInMemory();
+		createUsersUseCase = new CreateUsersUseCase(usersRepositoryInMemory);
+		authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory);
+	});
+	it('should be able create an token', () => {});
+});
