1fe738565124955f4ef6ece4a903c9c888ddb1c0
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-01 17:27:52 -0300

    fix: should be able to authenticated an user

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { ICreateUsersDTO } from '../../dtos/CreateUsersDTO';
 import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory';
 import { CreateUsersUseCase } from '../createUsers/CreateUsersUseCase';
 import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
@@ -12,5 +13,21 @@ describe('Authenticated user', () => {
 		createUsersUseCase = new CreateUsersUseCase(usersRepositoryInMemory);
 		authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory);
 	});
-	it('should be able create an token', () => {});
+	it('should be able to authenticated an user', async () => {
+		const user: ICreateUsersDTO = {
+			name: 'User1',
+			email: 'user@example.com',
+			password: '1234',
+			driver_license: 'DFE-1122',
+		};
+
+		await createUsersUseCase.execute(user);
+
+		const result = await authenticateUserUseCase.execute({
+			email: user.email,
+			password: user.password,
+		});
+
+		expect(result).toHaveProperty('token');
+	});
 });
