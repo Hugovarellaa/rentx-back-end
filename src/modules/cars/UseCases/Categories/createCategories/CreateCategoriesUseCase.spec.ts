2e38e52b9c045984ab0b2ca1c1d8b980e511bf35
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-07 15:48:51 -0300

    fix: should be able to create a new Category

--- src/modules/cars/UseCases/Categories/createCategories/CreateCategoriesUseCase.spec.ts
+++ src/modules/cars/UseCases/Categories/createCategories/CreateCategoriesUseCase.spec.ts
@@ -1,7 +1,28 @@
-import { ICategoriesRepository } from '../../../repositories/ICategoriesRepository';
+import { CategoriesRepositoryInMemory } from '../../../repositories/implementations/in-memory/CategoriesRepositoryInMemory';
+import { CreateCategoriesUseCase } from './CreateCategoriesUseCase';
 
-let categoriesRepositories: ICategoriesRepository;
+let createCategoriesUseCase: CreateCategoriesUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
 
 describe('Create Category', () => {
-	it('should be able to create a new Category', () => {});
+	beforeEach(() => {
+		categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+		createCategoriesUseCase = new CreateCategoriesUseCase(categoriesRepositoryInMemory);
+	});
+
+	it('should be able to create a new Category', async () => {
+		const category = {
+			name: 'Category Test',
+			description: 'Test description',
+		};
+
+		await createCategoriesUseCase.execute({
+			name: category.name,
+			description: category.description,
+		});
+
+		const createdCategory = await categoriesRepositoryInMemory.findByName(category.name);
+
+		expect(createdCategory).toHaveProperty('id');
+	});
 });
