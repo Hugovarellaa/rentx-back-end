1aabd64cd2a7b3ed2b3ecb91208e6dcddc1be696
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-07 15:31:20 -0300

    fix: create repository in memory

---
+++ src/modules/cars/repositories/implementations/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,23 @@
+import { ICreateCategoriesDTO } from '../../../dtos/ICreateCategoriesDTO';
+import { Category } from '../../../entities/Category';
+import { ICategoriesRepository } from '../../ICategoriesRepository';
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+	categories: Category[];
+
+	async create(data: ICreateCategoriesDTO): Promise<void> {
+		const { name, description } = data;
+		const category = new Category();
+		Object.assign(category, { name, description });
+
+		this.categories.push(category);
+	}
+	getAll(): Promise<Category[]> {
+		throw new Error('Method not implemented.');
+	}
+	findByName(name: string): Promise<Category> {
+		throw new Error('Method not implemented.');
+	}
+}
+
+export { CategoriesRepositoryInMemory };
