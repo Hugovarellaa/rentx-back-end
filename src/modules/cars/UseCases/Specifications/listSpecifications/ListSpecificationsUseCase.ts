7526d057da8c382c7eec27945e98adbf302bcd7e
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-11 12:59:09 -0300

    refactor: implementations

--- src/modules/cars/UseCases/Specifications/listSpecifications/ListSpecificationsUseCase.ts
+++ src/modules/cars/UseCases/Specifications/listSpecifications/ListSpecificationsUseCase.ts
@@ -1,13 +1,13 @@
 import { inject, injectable } from 'tsyringe';
 
 import { Specification } from '../../../entities/Specification';
-import { SpecificationsRepository } from '../../../repositories/implementations/SpecificationsRepository';
+import { ISpecificationsRepository } from '../../../repositories/ISpecificationsRepository';
 
 @injectable()
 class ListSpecificationsUseCase {
 	constructor(
 		@inject('SpecificationsRepository')
-		private specificationsRepository: SpecificationsRepository,
+		private specificationsRepository: ISpecificationsRepository,
 	) {}
 
 	async execute(): Promise<Specification[]> {
