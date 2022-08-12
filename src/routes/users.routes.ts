3079fc4ef41994ed96263ce21cca186b858693bd
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-12 12:34:27 -0300

    fix: create route updateUserAvatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import { CreateUsersController } from '../modules/accounts/useCases/createUsers/CreateUsersController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const usersRoutes = Router();
 const createUsersController = new CreateUsersController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUsersController.handle);
+usersRoutes.patch('/avatar', updateUserAvatarController.handle);
 
 export { usersRoutes };
