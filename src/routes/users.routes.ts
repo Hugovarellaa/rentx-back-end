3aa9e696481c9e4a55d8e6cc83a7bb5432dd3902
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-12 12:48:36 -0300

    refactor: uploadAvatar file

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 import multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import { CreateUsersController } from '../modules/accounts/useCases/createUsers/CreateUsersController';
 import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const usersRoutes = Router();
 const createUsersController = new CreateUsersController();
 const updateUserAvatarController = new UpdateUserAvatarController();
-const upload = multer({ dest: 'uploads/avatar' });
+
+const uploadAvatar = multer(uploadConfig.upload('./uploads/avatar'));
 
 usersRoutes.post('/', createUsersController.handle);
-usersRoutes.patch('/avatar', upload.single('avatar'), updateUserAvatarController.handle);
+usersRoutes.patch('/avatar', uploadAvatar.single('avatar'), updateUserAvatarController.handle);
 
 export { usersRoutes };
