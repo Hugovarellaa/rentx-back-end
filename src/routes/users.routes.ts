d8563577e12903e0e1538337c44e5db79575b54a
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-12 12:43:56 -0300

    fix: config upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CreateUsersController } from '../modules/accounts/useCases/createUsers/CreateUsersController';
 import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
@@ -6,8 +7,9 @@ import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateU
 const usersRoutes = Router();
 const createUsersController = new CreateUsersController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const upload = multer({ dest: 'uploads/avatar' });
 
 usersRoutes.post('/', createUsersController.handle);
-usersRoutes.patch('/avatar', updateUserAvatarController.handle);
+usersRoutes.patch('/avatar', upload.single('avatar'), updateUserAvatarController.handle);
 
 export { usersRoutes };
