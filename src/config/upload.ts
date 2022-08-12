5f4cac2093d0aba7415c3153ba0d0c803e3e09b8
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-12 12:48:13 -0300

    refactor: add retun function

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -4,14 +4,16 @@ import { resolve } from 'path';
 
 export default {
 	upload(folder: string) {
-		multer.diskStorage({
-			destination: resolve(__dirname, '..', '..', folder),
-			filename(req, file, callback) {
-				const fileHash = crypto.randomBytes(16).toString('hex');
-				const fileName = `${fileHash}-${file.originalname}`;
+		return {
+			storage: multer.diskStorage({
+				destination: resolve(__dirname, '..', '..', folder),
+				filename(req, file, callback) {
+					const fileHash = crypto.randomBytes(16).toString('hex');
+					const fileName = `${fileHash}-${file.originalname}`;
 
-				return callback(null, fileName);
-			},
-		});
+					return callback(null, fileName);
+				},
+			}),
+		};
 	},
 };
