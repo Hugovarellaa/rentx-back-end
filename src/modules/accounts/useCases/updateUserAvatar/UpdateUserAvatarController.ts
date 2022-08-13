a40faa51c2bf19cfe4f2568dd4edb7fd4b2c82bb
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-13 12:30:11 -0300

    fix: finish controller updateUser

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { UpdateUserAvatarUseCase } from './UpdateUserAvatarUseCase';
 
 class UpdateUserAvatarController {
-	async handle(request: Request, response: Response) {
+	async handle(request: Request, response: Response): Promise<Response> {
 		const avatar = request.file.filename;
-		const {} = request.user;
+		const { id } = request.user;
+
+		const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
+		await updateUserAvatarUseCase.execute({ user_id: id, avatar });
+
+		return response.status(204).send();
 	}
 }
 
