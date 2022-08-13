f7964044333d97b55f816af8a7349dfe6858a521
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-13 12:25:49 -0300

    fix: create update user avatar controller

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 
 class UpdateUserAvatarController {
-	async handle(request: Request, response: Response) {}
+	async handle(request: Request, response: Response) {
+		const avatar = request.file.filename;
+		const {} = request.user;
+	}
 }
 
 export { UpdateUserAvatarController };
