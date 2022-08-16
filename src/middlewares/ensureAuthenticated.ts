a31b9176a1048ff61395ab0554ada0a111e84513
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-16 11:38:52 -0300

    fix: treatment errors

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import { AppError } from '../errors/AppError';
 import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
 
 interface IPayload {
@@ -11,7 +12,7 @@ export async function ensureAuthenticated(request: Request, response: Response, 
 	const authHeader = request.headers.authorization;
 
 	if (!authHeader) {
-		throw new Error(`Token missing authorization`);
+		throw new AppError(`Token missing authorization`);
 	}
 
 	const [, token] = authHeader.split(' ');
