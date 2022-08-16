eaa44a0e162e02dd20746c1af7247c9bce9911f4
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-16 12:08:45 -0300

    fix: create update user avatar Controller

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,3 +1,7 @@
-class UpdateUserAvatarController {}
+import { Request, Response } from 'express';
+
+class UpdateUserAvatarController {
+	async handle(request: Request, response: Response) {}
+}
 
 export { UpdateUserAvatarController };
