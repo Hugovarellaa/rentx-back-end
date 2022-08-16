9129a70683a9ec73e303fa8a9a8656d63cda70ee
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-16 11:49:21 -0300

    fix: treatment errors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'reflect-metadata';
 import swaggerUi from 'swagger-ui-express';
 
+import 'express-async-errors';
 import './database';
+import { AppError } from './errors/AppError';
 import { router } from './routes';
 import './shared/container';
 import swaggerDocument from './swagger.json';
@@ -12,5 +14,18 @@ app.use(express.json());
 app.use(router);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	if (err instanceof AppError) {
+		return res.status(err.statusCode).json({
+			message: err.message,
+		});
+	}
+	return res.status(500).json({
+		status: 'error',
+		message: `Internal Server Error ${err.message}`,
+	});
+});
 
 app.listen(8080, () => console.log('listening on port 8080'));
