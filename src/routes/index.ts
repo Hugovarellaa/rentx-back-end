c56f6c03bf768dbf71c6388ca0aa69665f69eb56
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-18 10:57:45 -0300

    fix: create route session

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
+import { authenticatesRoutes } from './autenticates.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
 
 const router = Router();
+
+router.use(authenticatesRoutes);
 
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
