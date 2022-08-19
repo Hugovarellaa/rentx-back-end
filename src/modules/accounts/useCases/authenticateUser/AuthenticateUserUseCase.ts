8d08412fb52907c1e0c0d031ed32e868e9147528
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-19 10:54:15 -0300

    fix: create authenticate useCase

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,3 +1,58 @@
-class AuthenticateUserUseCase {}
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+
+interface IRequest {
+	email: string;
+	password: string;
+}
+
+interface IResponse {
+	user: {
+		name: string;
+		email: string;
+	};
+	token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+	constructor(
+		@inject('UsersRepository')
+		private usersRepository: UsersRepository,
+	) {}
+
+	async execute({ email, password }: IRequest): Promise<IResponse> {
+		// User exists
+		const user = await this.usersRepository.findByEmail(email);
+		if (!user) {
+			throw new Error(`Email or Password not incorrect`);
+		}
+
+		// Password they are the same
+		const passwordMatch = await compare(password, user.password);
+		if (!passwordMatch) {
+			throw new Error(`Email or Password not incorrect`);
+		}
+
+		// create jsonwebtoken
+		const token = sign({}, 'supersecret123', {
+			subject: user.id,
+			expiresIn: '1d',
+		});
+
+		const tokenReturn: IResponse = {
+			user: {
+				name: user.name,
+				email: user.email,
+			},
+			token,
+		};
+
+		return tokenReturn;
+	}
+}
 
 export { AuthenticateUserUseCase };
