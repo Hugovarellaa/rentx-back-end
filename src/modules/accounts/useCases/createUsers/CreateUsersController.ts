793010a1d5fdc82cc56b432f55effa9c7a40af9c
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-28 00:22:57 -0300

    fix: createUsersController

--- src/modules/accounts/useCases/createUsers/CreateUsersController.ts
+++ src/modules/accounts/useCases/createUsers/CreateUsersController.ts
@@ -1,3 +1,18 @@
-class CreateUsersController {}
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateUsersUseCase } from './CreateUsersUseCase';
+
+class CreateUsersController {
+	async handle(request: Request, response: Response): Promise<Response> {
+		const { name, email, password, driver_license, username } = request.body;
+
+		const createUsersUseCase = container.resolve(CreateUsersUseCase);
+
+		await createUsersUseCase.execute({ name, email, password, driver_license, username });
+
+		return response.status(201).send();
+	}
+}
 
 export { CreateUsersController };
