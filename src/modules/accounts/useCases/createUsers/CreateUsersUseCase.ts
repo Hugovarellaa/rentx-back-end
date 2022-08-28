5d37672767304bfbe4d3fba85d9e568cdfeee563
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-28 00:19:02 -0300

    fix: createUsersUseCase

--- src/modules/accounts/useCases/createUsers/CreateUsersUseCase.ts
+++ src/modules/accounts/useCases/createUsers/CreateUsersUseCase.ts
@@ -1,3 +1,20 @@
-class CreateUsersUseCase {}
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUsersDTO } from '../../dtos/CreateUsersDTO';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+@injectable()
+class CreateUsersUseCase {
+	constructor(
+		@inject('UsersRepository')
+		private usersRepository: IUsersRepository,
+	) {}
+
+	async execute(data: ICreateUsersDTO): Promise<void> {
+		const { name, email, password, driver_license, username } = data;
+
+		await this.usersRepository.create({ name, email, password, driver_license, username });
+	}
+}
 
 export { CreateUsersUseCase };
