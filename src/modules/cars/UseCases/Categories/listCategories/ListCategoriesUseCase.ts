7e472dec85e5c82d8b0ffae5fe299264749664d9
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-09-11 18:59:51 -0300

    refactor: dependency injection

--- src/modules/cars/UseCases/Categories/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/UseCases/Categories/listCategories/ListCategoriesUseCase.ts
@@ -1,11 +1,17 @@
+import { inject, injectable } from 'tsyringe';
+
 import { Category } from '../../../entities/Category';
 import { CategoriesRepository } from '../../../repositories/implementations/CategoriesRepository';
 
+@injectable()
 class ListCategoriesUseCase {
-	constructor(private categoriesRepository: CategoriesRepository) {}
-	execute(): Category[] {
-		const categories = this.categoriesRepository.getAll();
-		return categories;
+	constructor(
+		@inject('CategoriesRepository')
+		private categoriesRepository: CategoriesRepository,
+	) {}
+
+	async execute(): Promise<Category[]> {
+		return this.categoriesRepository.getAll();
 	}
 }
 
