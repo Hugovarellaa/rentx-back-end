7546109bcf06cba441b2cbbdc044fc057b049d0d
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-09-12 18:50:16 -0300

    refactor: remove file

--- src/modules/cars/UseCases/Categories/createCategories/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoriesRepository } from '../../../repositories/implementations/CategoriesRepository';
-import { CreateCategoriesController } from './CreateCategoriesController';
-import { CreateCategoriesUseCase } from './CreateCategoriesUseCase';
-
-const categoriesRepositories = CategoriesRepository.getInstance();
-const createCategoriesUseCase = new CreateCategoriesUseCase(categoriesRepositories);
-const createCategoriesController = new CreateCategoriesController(createCategoriesUseCase);
-
-export { createCategoriesController };
