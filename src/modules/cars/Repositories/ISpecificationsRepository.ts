cc43723cbe76b08c034be3a0e655b6e6fbf506de
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-31 15:20:20 -0300

    Contrato ISpecification

--- src/modules/cars/Repositories/ISpecificationsRepository.ts
+++ src/modules/cars/Repositories/ISpecificationsRepository.ts
@@ -1,1 +1,10 @@
-interface ISpecificationsRepository {}
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+interface ISpecificationsRepository {
+  create({ name, description }: ICreateCategoryDTO): void;
+}
+
+export { ISpecificationsRepository, ICreateCategoryDTO };
--- src/modules/cars/Repositories/SpecificationsRepository.ts
+++
@@ -1,3 +0,0 @@
-class SpecificationsRepository {}
-
-export { SpecificationsRepository };
