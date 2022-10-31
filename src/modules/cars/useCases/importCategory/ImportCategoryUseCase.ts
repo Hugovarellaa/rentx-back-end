434df1a0dec7966fdbfe624c559e4bc97b7318c7
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-31 17:47:30 -0300

    Leitura do arquivo csv

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,12 +1,17 @@
-import { parse as csvParse } from "csv-parse";
+import { parse } from "csv-parse";
 import fs from "fs";
 
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    const stream = fs.createWriteStream(file.path);
+    const stream = fs.createReadStream(file.path);
 
-    const parseFile = csvParse();
+    const parseFile = parse();
+
     stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
