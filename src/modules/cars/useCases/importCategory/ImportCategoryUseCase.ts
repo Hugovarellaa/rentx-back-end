947ff4416ce1891d7244be7a7146ff82c4477c7c
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-31 17:45:25 -0300

    Recebendo arquivo e criando stream de leitura

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,12 @@
+import { parse as csvParse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createWriteStream(file.path);
+
+    const parseFile = csvParse();
+    stream.pipe(parseFile);
   }
 }
 
