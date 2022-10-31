a9e0e5bd3ed5ebef8462c5ef141aa977d3aabe73
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-31 17:58:55 -0300

    Salvando no banco de dados

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,17 +1,36 @@
 import { parse } from "csv-parse";
 import fs from "fs";
 
-class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
+import { ICategoriesRepository } from "../../Repositories/ICategoriesRepository";
 
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoryUseCase {
+  constructor(private categoryRepository: ICategoriesRepository) {}
+
+  loadCategory(file: Express.Multer.File): IImportCategory[] {
+    const stream = fs.createReadStream(file.path);
+    const categories: IImportCategory[] = [];
     const parseFile = parse();
 
     stream.pipe(parseFile);
 
     parseFile.on("data", async (line) => {
-      console.log(line);
+      const [name, description] = line;
+      categories.push({
+        name,
+        description,
+      });
     });
+    return categories;
+  }
+
+  execute(file: Express.Multer.File): void {
+    const categories = this.loadCategory(file);
+    console.log(categories);
   }
 }
 
