6f3c79ab97fb8288ebaf1c08afc655716287e19a
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-01 11:10:56 -0300

    Refatorando Repository + removendo Singleton + Salvando no banco de dados

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,8 +1,7 @@
-import { CategoriesRepository } from "../../Repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const categoryRepository = CategoriesRepository.getInstance();
+const categoryRepository = null;
 const importCategoryUseCase = new ImportCategoryUseCase(categoryRepository);
 const importCategoryController = new ImportCategoryController(
   importCategoryUseCase
