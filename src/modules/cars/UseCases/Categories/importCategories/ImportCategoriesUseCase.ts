683e388bb6504c4a710e8fdd87e2b4b707398ef7
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-02 16:15:46 -0300

    fix: update ImportCategories

--- src/modules/cars/UseCases/Categories/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/UseCases/Categories/importCategories/ImportCategoriesUseCase.ts
@@ -1,3 +1,8 @@
-class ImportCategoriesUseCase {}
+import { CategoriesRepository } from '../../../repositories/implementations/CategoriesRepository';
+
+class ImportCategoriesUseCase {
+	constructor(private categoriesRepository: CategoriesRepository) {}
+	execute(file: Express.Multer.File) {}
+}
 
 export { ImportCategoriesUseCase };
