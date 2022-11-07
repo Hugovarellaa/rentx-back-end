b4733651e4cd9086f38255c06af71efaec8bb284
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-07 11:48:32 -0300

    Criando CategoriesRepository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,10 @@
+import { Category } from '../model/Category'
+
+class CategoriesRepository {
+    private categories: Category[]
+    constructor() {
+        this.categories = []
+    }
+}
+
+export { CategoriesRepository }
