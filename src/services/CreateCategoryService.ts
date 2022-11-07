dbf920adbec29db15b5b49ab9fe2d4bed674b789
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-07 14:58:57 -0300

    Usando o conceito de services

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,5 +1,23 @@
+import { CategoriesRepository } from '../repositories/CategoriesRepository'
+
+interface IRequest {
+    name: string
+    description: string
+}
+
 class CreateCategoryService {
-    execute() {}
+    constructor(private categoriesRepositories: CategoriesRepository) {}
+
+    execute({ name, description }: IRequest) {
+        const categoriesAlreadyExists =
+            this.categoriesRepositories.findByName(name)
+
+        if (categoriesAlreadyExists) {
+            throw new Error(`Cannot create category`)
+        }
+
+        this.categoriesRepositories.create({ name, description })
+    }
 }
 
 export { CreateCategoryService }
