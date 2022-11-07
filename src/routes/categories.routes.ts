409ce0a3e6e65dccbc5bf4428b9904a14f6cfed0
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-07 11:29:40 -0300

    Diagrama + categories

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+
+const categoriesRoutes = Router()
+
+const categories = []
+
+categoriesRoutes.post('/', (request, response) => {
+    const { name, description } = request.body
+
+    const category = {
+        name,
+        description,
+    }
+
+    categories.push(category)
+
+    return response.status(201).json({ category })
+})
+
+export { categoriesRoutes }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from 'express'
 
+import { categoriesRoutes } from './routes/categories.routes'
+
 const app = express()
+
+app.use(express.json())
+app.use('/categories', categoriesRoutes)
 
 app.listen(3334, () => console.log('Server listening on port 3334'))
