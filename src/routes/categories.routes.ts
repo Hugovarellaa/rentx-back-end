eb85b3091e4a7f84f567df9405549c9a08c95c0f
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-08 16:11:42 -0300

    fix: route import categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoriesController } from '../modules/cars/UseCases/Categories/createCategories';
+import { importCategoriesController } from '../modules/cars/UseCases/Categories/importCategories';
 import { listCategoriesController } from '../modules/cars/UseCases/Categories/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({ dest: 'uploads/' });
 
 categoriesRoutes.post('/', (request, response) => {
 	createCategoriesController.handle(request, response);
@@ -11,6 +15,10 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', (request, response) => {
 	listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+	importCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
