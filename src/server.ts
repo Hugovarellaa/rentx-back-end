7d7bbb9fb22e1d224648bc4ebc9a023390831308
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-09 18:48:40 -0300

    Swagger docs

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 
+import swaggerDocument from '../swagger.json'
 import { router } from './routes'
 
 const app = express()
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(express.json())
 app.use(router)
 
