56917626797624398b518a40970843c418df7a32
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-10 16:12:44 -0300

    Gerando token JWT

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -1,5 +1,21 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase'
+
 class AuthenticateUserController {
-    handle() {}
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { email, password } = request.body
+
+        const authenticateUseCase = container.resolve(AuthenticateUserUseCase)
+
+        const token = await authenticateUseCase.execute({
+            password,
+            email
+        })
+
+        return response.json(token)
+    }
 }
 
 export { AuthenticateUserController }
--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,5 +1,51 @@
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+import { inject, injectable } from 'tsyringe'
+
+import { UsersRepository } from '../../repositories/implementations/UsersRepository'
+
+interface IRequest {
+    email: string
+    password: string
+}
+
+interface IResponse {
+    user: {
+        name: string
+        email: string
+    }
+    token: string
+}
+
+@injectable()
 class AuthenticateUserUseCase {
-    execute() {}
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: UsersRepository
+    ) {}
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        // Usuário existe
+
+        const user = await this.usersRepository.findByEmail(email)
+
+        if (!user) {
+            throw new Error(`Email or password incorrect`)
+        }
+
+        // Senha está correta
+        const passwordMatch = compare(password, user.password)
+        if (!passwordMatch) {
+            throw new Error(`Email or password incorrect`)
+        }
+
+        // Gerar JsonWebToken
+        const token = sign({}, '5f36f997eb9c08b73986a336dc3a7bb3', {
+            subject: user.id,
+            expiresIn: '1d'
+        })
+
+        return { user, token }
+    }
 }
 
 export { AuthenticateUserUseCase }
