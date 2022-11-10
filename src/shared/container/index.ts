c92e4a2a8ded232084cf0e6bfbc9b61694b36869
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-10 09:43:46 -0300

    Configurando e usando Tyringe em createSpecificationController

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,17 @@ import { container } from 'tsyringe'
 
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository'
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository'
+import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository'
+import { ISpecificationRepository } from '../../modules/cars/repositories/ISpecificationsRepository'
 
 // Singleton vai receber o ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
     'CategoriesRepository',
     CategoriesRepository
+)
+
+// Singleton vai receber o ISpecificationRepository
+container.registerSingleton<ISpecificationRepository>(
+    'SpecificationsRepository',
+    SpecificationsRepository
 )
