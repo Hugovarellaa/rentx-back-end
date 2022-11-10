27cd1f386da58df98e2f9f15be9d3e3b590eb073
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-09 23:39:16 -0300

    Mapeando tabela de categories

--- src/modules/cars/model/Category.ts
+++ src/modules/cars/model/Category.ts
@@ -1,12 +1,17 @@
+import { Column, CreateDateColumn, PrimaryColumn } from 'typeorm'
 import { v4 as uuidV4 } from 'uuid'
 
 class Category {
+    @PrimaryColumn()
     id?: string
 
+    @Column()
     name: string
 
+    @Column()
     description: string
 
+    @CreateDateColumn()
     created_ad: Date
 
     constructor() {
