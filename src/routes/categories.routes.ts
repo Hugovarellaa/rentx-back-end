a16a9463472e093db4fa0e1c4ece21562b18c728
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-10 09:36:35 -0300

    Configurando e usando Tyringe

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,16 @@
 import { Router } from 'express'
 import multer from 'multer'
 
-import createCategoryController from '../modules/cars/useCases/createCategory'
+import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController'
 import { importCategoriesController } from '../modules/cars/useCases/importCategories'
 import { listCategoryController } from '../modules/cars/useCases/listCategories'
 
 const upload = multer({ dest: 'uploads/' })
 
 const categoriesRoutes = Router()
+const createCategoryController = new CreateCategoryController()
 
-categoriesRoutes.post('/', (request, response) => {
-    return createCategoryController().handle(request, response)
-})
+categoriesRoutes.post('/', createCategoryController.handle)
 
 categoriesRoutes.get('/', (request, response) => {
     return listCategoryController.handle(request, response)
