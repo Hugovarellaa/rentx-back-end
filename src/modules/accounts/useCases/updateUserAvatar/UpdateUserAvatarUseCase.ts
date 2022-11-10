4e84af49d7fbafb607f41f60249b7100dc796529
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-10 20:12:03 -0300

    Adicionando tipagem extra ao express

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,8 +1,23 @@
+import { inject, injectable } from 'tsyringe'
+
+import { IUsersRepository } from '../../repositories/IUsersRepository'
+
+interface IRequest {
+    user_id: string
+    avatar_file: string
+}
+
+@injectable()
 class UpdateUserAvatarUseCase {
-    async execute() {
-        // Adicionar coluna avatar na tabela de users
-        // Refatorar usuário com coluna avatar
-        // Configuração upload multer
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository
+    ) {}
+    async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+        const user = await this.usersRepository.findById(user_id)
+        user.avatar = avatar_file
+
+        await this.usersRepository.create(user)
     }
 }
 
