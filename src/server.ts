2fdc367d149f9da4b05a9936850535a6e50a2ae5
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-09 21:06:23 -0300

    Config docker-compose + banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express'
+import 'reflect-metadata'
 import swaggerUi from 'swagger-ui-express'
+import './database'
 
 import swaggerDocument from '../swagger.json'
 import { router } from './routes'
