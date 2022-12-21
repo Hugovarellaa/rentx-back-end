f66ff24b5a3e5b8f495cbca94613ad5b8cbc6f38
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-21 19:40:00 -0300

    Add Prettier

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.listen(8080, () => console.log("Listening on port 8080!"));
+app.listen(8080, () => console.log('Listening on port 8080!'))
