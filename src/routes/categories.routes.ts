d34ccc0a7b4bc0848456aa5e5dc090771c73aa9f
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-25 20:43:09 -0300

    Refact categoriesServices

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,16 @@
 import { Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryServices } from '../services/CreateCategoryServices';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
+const createCategoryServices = new CreateCategoryServices(categoriesRepository);
 
 categoriesRoutes.post('/', (request, response) => {
 	const { name, description } = request.body;
-	const categoryAlreadyExists = categoriesRepository.findByName(name);
-	if (categoryAlreadyExists) {
-		return response.status(400).json({
-			error: 'Category already exists',
-		});
-	}
 
-	categoriesRepository.create({ name, description });
+	createCategoryServices.execute({ name, description });
 	return response.status(201).send();
 });
 
