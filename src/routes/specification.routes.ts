b34c3da453e978ca2b539033b1085ec22b663edd
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-26 11:53:04 -0300

    Refact with controller

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,20 +1,13 @@
 import { Router } from 'express';
 
 import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository';
-import { CreateSpecificationServices } from '../modules/cars/services/CreateSpecificationServices';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
 const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post('/', async (request, response) => {
-	const { name, description } = request.body;
-
-	const createSpecificationServices = new CreateSpecificationServices(
-		specificationsRepository,
-	);
-	createSpecificationServices.execute({ name, description });
-
-	return response.status(201).send();
+	createSpecificationController.handle(request, response);
 });
 
 specificationsRoutes.get('/', async (request, response) => {
