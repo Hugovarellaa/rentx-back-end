04d7862a041302fdf94e0f0d65bbeb87360ac981
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-25 21:43:59 -0300

    Execute CreateSpecificationsServices

--- src/modules/cars/services/CreateSpecificationServices.ts
+++ src/modules/cars/services/CreateSpecificationServices.ts
@@ -1,6 +1,14 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+	name: string;
+	description: string;
+}
+
 class CreateSpecificationServices {
-	execute() {
-		console.log('createSpecificationServices');
+	constructor(private specificationRepository: ISpecificationsRepository) {}
+	execute({ name, description }: IRequest) {
+		return this.specificationRepository.create({ name, description });
 	}
 }
 
