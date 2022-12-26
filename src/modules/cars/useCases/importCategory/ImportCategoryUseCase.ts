49145b0cc28cd357fa55a938c60e51ea4e3d5a7f
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-26 12:36:18 -0300

    instance signleton pattern

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,10 @@
 import { parse as csvParse } from 'csv-parse';
 import fs from 'fs';
 
+import { ICategoryRepository } from '../../repositories/ICategoryRepository';
+
 class ImportCategoryUseCase {
+	constructor(private categoriesRepository: ICategoryRepository) {}
 	execute(file: Express.Multer.File) {
 		const stream = fs.createReadStream(file.path);
 		const parseFile = csvParse();
