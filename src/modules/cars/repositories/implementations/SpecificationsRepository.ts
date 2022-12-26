4fa7070c190fe61b424e11917116fd63dcfc9120
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-25 21:35:18 -0300

    Interface ISpecificationsRepository

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,3 +1,22 @@
-class SpecificationsRepository {}
+import { Specification } from '../../entities/Specification';
+import {
+	ICreateSpecificationDTO,
+	ISpecificationsRepository,
+} from '../ISpecificationsRepository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+	private specifications: Specification[];
+
+	constructor() {
+		this.specifications = [];
+	}
+
+	create({ name, description }: ICreateSpecificationDTO): void {
+		const specification = new Specification();
+		Object.assign(specification, { name, description });
+
+		this.specifications.push(specification);
+	}
+}
 
 export { SpecificationsRepository };
