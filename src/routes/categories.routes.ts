db28692f99ed439f84f5d1014480b0c10e8bd5bf
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-27 10:46:28 -0300

    Using container tsyring

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,16 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import createCategoryController from '../modules/cars/useCases/createCategory';
+import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
 import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategories';
 
 const upload = multer({ dest: 'uploads/' });
+const createCategoryController = new CreateCategoryController();
 
 const categoriesRoutes = Router();
 
-categoriesRoutes.post('/', (request, response) => {
-	createCategoryController().handle(request, response);
-});
+categoriesRoutes.post('/', createCategoryController.handle);
 
 categoriesRoutes.get('/', (request, response) => {
 	listCategoryController.handle(request, response);
