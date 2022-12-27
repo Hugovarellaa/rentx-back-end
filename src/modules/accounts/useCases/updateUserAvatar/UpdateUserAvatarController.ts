df42d23a64c1f22f301b549a5e6cfa0dee7875e1
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-27 20:30:27 -0300

    Add table column avatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -0,0 +1,5 @@
+class UpdateUserAvatarController {
+	handle() {}
+}
+
+export { UpdateUserAvatarController };
---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,10 @@
+class UpdateUserAvatarUseCase {
+	async execute() {
+		// Adicionar coluna avatar na tabela de users
+		// Refatora o usuário com coluna avatar
+		// Configuração upload multer
+		// Criar a regra de negocio do upload
+	}
+}
+
+export { UpdateUserAvatarUseCase };
