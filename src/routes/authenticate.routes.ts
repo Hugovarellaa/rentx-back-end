7b2d08e86442174be7b71f7ea42fce24dd34c62a
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-27 17:12:28 -0300

    Routes sessions

--- src/routes/authenticate.routes.ts
+++ src/routes/authenticate.routes.ts
@@ -5,6 +5,6 @@ import { AuthenticateUserController } from '../modules/accounts/useCases/authent
 const authenticateRoutes = Router();
 const authenticateUserController = new AuthenticateUserController();
 
-authenticateRoutes.post('/session', authenticateUserController.handle);
+authenticateRoutes.post('/sessions', authenticateUserController.handle);
 
 export { authenticateRoutes };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import { authenticateRoutes } from './authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specification.routes';
 import { usersRoutes } from './urses.routes';
 
 const router = Router();
 
+router.use(authenticateRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/specification', specificationsRoutes);
 router.use('/users', usersRoutes);
