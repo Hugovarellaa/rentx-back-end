146fa27cc32f5b9df649e8c838d380ffb9203e65
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-28 13:31:56 -0300

    refactor: list specification - Controller

--- src/UseCases/Specifications/listSpecifications/ListSpecificationsController.ts
+++ src/UseCases/Specifications/listSpecifications/ListSpecificationsController.ts
@@ -1,3 +1,14 @@
-class ListSpecificationsController {}
+import { Request, Response } from 'express';
+
+import { ListSpecificationsUseCase } from './ListSpecificationsUseCase';
+
+class ListSpecificationsController {
+	constructor(private listSpecificationsUseCase: ListSpecificationsUseCase) {}
+	handle(request: Request, response: Response) {
+		const specifications = this.listSpecificationsUseCase.execute();
+
+		return response.json(specifications);
+	}
+}
 
 export { ListSpecificationsController };
