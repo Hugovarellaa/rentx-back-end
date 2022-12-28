be211ca8f267518a50751b444b553a7979d2c07b
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-28 13:30:29 -0300

    refactor: list specification - UseCase

--- src/UseCases/Specifications/listSpecifications/ListSpecificationsUseCase.ts
+++ src/UseCases/Specifications/listSpecifications/ListSpecificationsUseCase.ts
@@ -1,3 +1,12 @@
-class ListSpecificationsUseCase {}
+import { Specification } from '../../../entities/Specification';
+import { SpecificationsRepository } from '../../../repositories/implementations/SpecificationsRepository';
+
+class ListSpecificationsUseCase {
+	constructor(private specificationsRepository: SpecificationsRepository) {}
+
+	execute(): Specification[] {
+		return this.specificationsRepository.getAll();
+	}
+}
 
 export { ListSpecificationsUseCase };
