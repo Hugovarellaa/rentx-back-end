a4594226a17eecb3d1508efc87f5f2b0da6f7f60
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-28 13:34:45 -0300

    refactor: Add route get - Specifications

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
 import { createSpecificationsController } from '../UseCases/Specifications/createSpecifications';
+import { listSpecificationsController } from '../UseCases/Specifications/listSpecifications';
 
 const specificationsRoutes = Router();
 
 specificationsRoutes.post('/', (request, response) => {
 	createSpecificationsController.handle(request, response);
+});
+
+specificationsRoutes.get('/', (request, response) => {
+	listSpecificationsController.handle(request, response);
 });
 
 export { specificationsRoutes };
