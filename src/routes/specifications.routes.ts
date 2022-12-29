fbc0c45e895f10e8f14fdbc2144546ad06eaaf11
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-29 13:25:35 -0300

    refactor: create route POST specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import { createSpecificationsController } from '../UseCases/Specifications/createSpecifications';
+
 const specificationsRoutes = Router();
+
+specificationsRoutes.post('/', (request, response) => {
+	createSpecificationsController.handle(request, response);
+});
 
 export { specificationsRoutes };
