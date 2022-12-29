c606fd6d9c71edf849384ffba7db8fddefe79a26
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-29 13:22:32 -0300

    refactor: CreateController - Specification

--- src/UseCases/Specifications/createSpecifications/CreateSpecificationsController.ts
+++ src/UseCases/Specifications/createSpecifications/CreateSpecificationsController.ts
@@ -1,3 +1,16 @@
-class CreateSpecificationsController {}
+import { Request, Response } from 'express';
+
+import { CreateSpecificationsUseCase } from './CreateSpecificationsUseCase';
+
+class CreateSpecificationsController {
+	constructor(private createSpecificationsUseCase: CreateSpecificationsUseCase) {}
+	handle(request: Request, response: Response): Response {
+		const { name, description } = request.body;
+
+		this.createSpecificationsUseCase.execute({ name, description });
+
+		return response.status(200).send();
+	}
+}
 
 export { CreateSpecificationsController };
