047ef237c83a9fa7933f4b39418238eca54bcf86
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-29 13:20:43 -0300

    refactor: CreateUseCase - Specification

--- src/UseCases/Specifications/createSpecifications/CreateSpecificationsUseCase.ts
+++ src/UseCases/Specifications/createSpecifications/CreateSpecificationsUseCase.ts
@@ -1,3 +1,21 @@
-class CreateSpecificationsUseCase {}
+import { SpecificationsRepository } from '../../../repositories/implementations/SpecificationsRepository';
+
+interface IRequest {
+	name: string;
+	description: string;
+}
+
+class CreateSpecificationsUseCase {
+	constructor(private specificationsRepository: SpecificationsRepository) {}
+	execute({ name, description }: IRequest) {
+		const specificationsAlreadyExists = this.specificationsRepository.findByName(name);
+
+		if (specificationsAlreadyExists) {
+			throw new Error(`Specifications already exists`);
+		}
+
+		this.specificationsRepository.create({ name, description });
+	}
+}
 
 export { CreateSpecificationsUseCase };
