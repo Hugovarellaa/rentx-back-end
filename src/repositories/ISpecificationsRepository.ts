53ac40667bae44a16a36450616cb85ac7de9cd39
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-31 13:08:08 -0300

    refactor: update method specification repository

--- src/repositories/ISpecificationsRepository.ts
+++ src/repositories/ISpecificationsRepository.ts
@@ -1,5 +1,10 @@
+import { ICreateSpecificationsDTO } from '../dtos/ICreateSpecificationsDTO';
+import { Specification } from '../entities/Specification';
+
 interface ISpecificationsRepository {
-	create(): void;
+	create({ name, description }: ICreateSpecificationsDTO): void;
+	getAll(): Specification[];
+	findByName(name: string): Specification;
 }
 
 export { ISpecificationsRepository };
