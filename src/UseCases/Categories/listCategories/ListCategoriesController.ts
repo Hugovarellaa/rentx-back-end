79d65b4fcb9972afcc4232a2f84ef955772fe491
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-22 12:49:08 -0300

    fix: controller list categories

--- src/UseCases/Categories/listCategories/ListCategoriesController.ts
+++ src/UseCases/Categories/listCategories/ListCategoriesController.ts
@@ -1,3 +1,15 @@
-class ListCategoriesController {}
+import { Request, Response } from 'express';
+
+import { ListCategoriesUseCase } from './ListCategoriesUseCase';
+
+class ListCategoriesController {
+	constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
+
+	handle(request: Request, response: Response): Response {
+		const categories = this.listCategoriesUseCase.execute();
+
+		return response.json(categories);
+	}
+}
 
 export { ListCategoriesController };
