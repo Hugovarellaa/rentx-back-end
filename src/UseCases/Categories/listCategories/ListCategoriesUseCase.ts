67b81409f001011e3e8f8e1a34c119e9501d3714
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-22 12:47:35 -0300

    fix: useCase list categories

--- src/UseCases/Categories/listCategories/ListCategoriesUseCase.ts
+++ src/UseCases/Categories/listCategories/ListCategoriesUseCase.ts
@@ -1,3 +1,10 @@
-class ListCategoriesUseCase {}
+import { CategoriesRepository } from '../../../repositories/implementations/CategoriesRepository';
+
+class ListCategoriesUseCase {
+	constructor(private categoriesRepository: CategoriesRepository) {}
+	execute(): void {
+		this.categoriesRepository.getAll();
+	}
+}
 
 export { ListCategoriesUseCase };
