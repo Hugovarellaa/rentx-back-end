78a4d0ddb5fde5228c6ed2355baf60a24dbc3776
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-23 20:12:11 -0300

    using CategoriesRepositoryServices

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,15 @@
 import { Response, Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response): Response => {
 	const { name, description } = request.body;
-
-	const categoriesAlreadyExists = categoriesRepository.findByName(name);
-
-	if (categoriesAlreadyExists) {
-		return response.status(404).json({ message: 'Categories Already Exists' });
-	}
-
-	categoriesRepository.create({ name, description });
-
+	const createCategoryService = new CreateCategoryService(categoriesRepository);
+	createCategoryService.execute({ name, description });
 	return response.status(201).send();
 });
 
@@ -24,4 +18,5 @@ categoriesRoutes.get('/', (request, response): Response => {
 
 	return response.json(categoriesAll);
 });
+
 export { categoriesRoutes };
