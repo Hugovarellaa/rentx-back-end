00edfa4594cbe1d80fbec46d85288704aded230d
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-23 17:41:08 -0300

    Validation categories already exists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,11 @@ const categories = [];
 categoriesRoutes.post('/', (request, response): Response => {
 	const { name, description } = request.body;
 
+	const categoriesAlreadyExists = categories.find((category) => category.name === name);
+	if (categoriesAlreadyExists) {
+		return response.status(400).json({ message: 'Categories already exists' });
+	}
+
 	const category = {
 		id: uuidV4(),
 		name,
@@ -17,7 +22,7 @@ categoriesRoutes.post('/', (request, response): Response => {
 
 	categories.push(category);
 
-	return response.status(201).json({ categories });
+	return response.status(201).send();
 });
 
 export { categoriesRoutes };
