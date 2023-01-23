28602f1b4fb7750f3b010e87dc876e93f688414f
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-23 17:32:27 -0300

    Routes create categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,23 @@
 import { Response, Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
+const categories = [];
+
 categoriesRoutes.post('/', (request, response): Response => {
-	return response.status(201).send();
+	const { name, description } = request.body;
+
+	const category = {
+		id: uuidV4(),
+		name,
+		description,
+		created_at: new Date(),
+	};
+
+	categories.push(category);
+
+	return response.status(201).json({ categories });
 });
 
 export { categoriesRoutes };
