f0192a0aea43e2544b15ef273610b5ff522c0629
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-24 19:15:04 -0300

    Refact Inject singleton

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
-import { importCategoriesController } from '../modules/cars/useCases/importCategories';
+import { ImportCategoriesController } from '../modules/cars/useCases/importCategories/ImportCategoriesController';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const upload = multer({ dest: 'uploads/' });
 
 const categoriesRoutes = Router();
 const createCategoryController = new CreateCategoryController();
+const importCategoriesController = new ImportCategoriesController();
 
 categoriesRoutes.post('/', createCategoryController.handle);
 
@@ -16,8 +17,6 @@ categoriesRoutes.get('/', (request, response) => {
 	return listCategoriesController.handle(request, response);
 });
 
-categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-	return importCategoriesController.handle(request, response);
-});
+categoriesRoutes.post('/import', upload.single('file'), importCategoriesController.handle);
 
 export { categoriesRoutes };
