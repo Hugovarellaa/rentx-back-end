21cb6fe89a7e04bb047f834559dc2aa9bc463afc
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-24 19:18:16 -0300

    Refact Inject singleton

--- src/modules/cars/useCases/listCategories/index.ts
+++
@@ -1,8 +0,0 @@
-import { ListcategoriesController } from './ListCategoriesController';
-import { ListCategoriesUseCase } from './ListCategoriesUseCase';
-
-const categoriesRepository = null;
-const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
-const listCategoriesController = new ListcategoriesController(listCategoriesUseCase);
-
-export { listCategoriesController };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,20 +3,17 @@ import multer from 'multer';
 
 import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
 import { ImportCategoriesController } from '../modules/cars/useCases/importCategories/ImportCategoriesController';
-import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import { ListcategoriesController } from '../modules/cars/useCases/listCategories/ListCategoriesController';
 
 const upload = multer({ dest: 'uploads/' });
 
 const categoriesRoutes = Router();
 const createCategoryController = new CreateCategoryController();
 const importCategoriesController = new ImportCategoriesController();
+const listCategoriesController = new ListcategoriesController();
 
 categoriesRoutes.post('/', createCategoryController.handle);
-
-categoriesRoutes.get('/', (request, response) => {
-	return listCategoriesController.handle(request, response);
-});
-
+categoriesRoutes.get('/', listCategoriesController.handle);
 categoriesRoutes.post('/import', upload.single('file'), importCategoriesController.handle);
 
 export { categoriesRoutes };
