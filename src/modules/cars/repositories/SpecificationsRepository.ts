6eddb46f129941c9f0ec50fe4d74771a6cd88660
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-23 21:00:51 -0300

    Repository Specifications

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,20 @@
+import { Specification } from '../entities/Specification';
+import { ICreateSpecificationDTO, ISpecificationsRepository } from './implementations/ISpecificationsRepository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+	private specification: Specification[];
+	constructor() {
+		this.specification = [];
+	}
+	create({ name, description }: ICreateSpecificationDTO): void {
+		const specification = new Specification();
+		Object.assign(specification, {
+			name,
+			description,
+			created_at: new Date(),
+		});
+		this.specification.push(specification);
+	}
+}
+
+export { SpecificationsRepository };
