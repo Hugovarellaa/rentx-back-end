0212d5cef5521b7a0c3f4b7db2a04faa0571db8c
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-24 11:41:16 -0300

    refact routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specification.routes';
+import { router } from './routes';
 
 const PORT = 8080;
+
 const app = express();
 app.use(express.json());
-
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
+app.use(router);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
