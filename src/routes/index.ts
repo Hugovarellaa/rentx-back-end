d7f899da1371f139172c050d0f40e6fb53f69eb7
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-25 14:50:45 -0300

    authenticateRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
+import { authenticateRoutes } from './authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specification.routes';
 import { usersRoutes } from './users.routes';
 
 const router = Router();
+
+router.use(authenticateRoutes);
 
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
