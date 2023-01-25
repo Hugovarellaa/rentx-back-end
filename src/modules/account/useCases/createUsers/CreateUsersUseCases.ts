bc33c0ecdcf6ee1e7a39b5998b35699d329ec94b
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-25 11:48:54 -0300

    CreateUsersUseCase

--- src/modules/account/useCases/createUsers/CreateUsersUseCases.ts
+++ src/modules/account/useCases/createUsers/CreateUsersUseCases.ts
@@ -1,3 +1,18 @@
-class CreateUsersUseCase {}
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+
+@injectable()
+class CreateUsersUseCase {
+	constructor(
+		@inject('UsersRepository')
+		private usersRepository: UsersRepository,
+	) {}
+	async execute(data: ICreateUserDTO): Promise<void> {
+		const { name, email, password, driver_license } = data;
+		await this.usersRepository.create({ name, email, password, driver_license });
+	}
+}
 
 export { CreateUsersUseCase };
