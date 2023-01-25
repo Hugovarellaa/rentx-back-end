d26a62c5dd7dd260923eba193ae291d0f8569715
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-25 11:48:11 -0300

    Create container singleton UsersRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import { UsersRepository } from '../../modules/account/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '../../modules/account/repositories/IUsersRepository';
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
@@ -10,3 +12,6 @@ container.registerSingleton<ICategoriesRepository>('CategoriesRepository', Categ
 
 // (Interface no generic do singleton) ISpecificationsRepository / Definir um nome pro container / Class que é chamada quando o nome definido for chamado
 container.registerSingleton<ISpecificationsRepository>('SpecificationsRepository', SpecificationsRepository);
+
+// (Interface no generic do singleton) IUsersRepository / Definir um nome pro container / Class que é chamada quando o nome definido for chamado
+container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
