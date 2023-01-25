21430b57c3f194ec99113b55b7b306633433a1eb
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-25 14:37:05 -0300

    AuthenticateUserUseCase

---
+++ src/modules/account/useCases/authenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+class AuthenticateUserController {
+	handle(request: Request, response: Response) {
+		console.log('ok');
+	}
+}
+
+export { AuthenticateUserController };
---
+++ src/modules/account/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,52 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+
+interface IRequest {
+	email: string;
+	password: string;
+}
+
+interface IResponse {
+	user: {
+		name: string;
+		email: string;
+	};
+	token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+	constructor(
+		@inject('UsersRepository')
+		private usersRepository: UsersRepository,
+	) {}
+	async execute({ email, password }: IRequest): Promise<IResponse> {
+		// Usuário existir?
+		const user = await this.usersRepository.findByEmail(email);
+		if (!user) {
+			throw new Error('Email or password incorrect!');
+		}
+
+		// Senha correta?
+		const passwordMatch = compare(password, user.password);
+		if (!passwordMatch) {
+			throw new Error('Email or password incorrect!');
+		}
+
+		// Gerar token
+		const token = sign({}, 'supersecret123', {
+			subject: user.id,
+			expiresIn: '1d',
+		});
+
+		return {
+			user,
+			token,
+		};
+	}
+}
+
+export { AuthenticateUserUseCase };
