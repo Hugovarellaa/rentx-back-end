c4356b8adc6eee06c000637bba78c585a37a13b3
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-05 12:29:22 -0300

    fix: create method in memory createCategories

--- src/repositories/implementations/CategoriesRepository.ts
+++ src/repositories/implementations/CategoriesRepository.ts
@@ -1,3 +1,27 @@
-class CategoriesRepository {}
+import { ICreateCategoriesDTO } from '../../dtos/ICreateCategoriesDTO';
+import { Category } from '../../entities/Category';
+import { ICategoriesRepository } from '../ICategoriesRepository';
+
+class CategoriesRepository implements ICategoriesRepository {
+	categories: Category[];
+
+	constructor() {
+		this.categories = [];
+	}
+
+	create(data: ICreateCategoriesDTO): void {
+		const { name, description } = data;
+		const category = new Category();
+		Object.assign(category, { name, description });
+		this.categories.push(category);
+	}
+
+	getAll(): Category[] {
+		throw new Error('Method not implemented.');
+	}
+	findByName(name: string): Category {
+		throw new Error('Method not implemented.');
+	}
+}
 
 export { CategoriesRepository };
