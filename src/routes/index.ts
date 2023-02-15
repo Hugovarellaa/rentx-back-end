07dcc6a7c4313c4d1182aa89396116a7ebf1aecc
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-15 16:52:43 -0300

    Delete src directory

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { categoriesRoutes } from './categoriesRoutes.routes';
-
-const router = Router();
-
-router.use('/categories', categoriesRoutes);
-
-export { router };
