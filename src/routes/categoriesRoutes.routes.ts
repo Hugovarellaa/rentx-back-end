f74981c65d8fdd25f8abf6fb5e4efcc8ff05008c
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-15 12:27:29 -0300

    register information with method post

--- src/routes/categoriesRoutes.routes.ts
+++ src/routes/categoriesRoutes.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRoutes = Router();
+
+const categories = [];
 
 categoriesRoutes.post('/', (request, response) => {
 	const { name, description } = request.body;
 
-	return response.json({ name, description });
+	const category = {
+		id: uuidV4(),
+		name,
+		description,
+		created_at: new Date(),
+	};
+
+	categories.push(category);
+
+	return response.json({ category });
 });
 
 export { categoriesRoutes };
