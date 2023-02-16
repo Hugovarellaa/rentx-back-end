43448c131535dbee722062eaf12b5911dc377c18
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-16 18:03:34 -0300

    Add entities and using

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,18 @@
 import { Router } from 'express';
 
+import { Category } from '../entities/Category';
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request, response) => {
 	const { name, description } = request.body;
 
-	const category = {
-		name,
-		description,
-	};
+	const category = new Category();
+	Object.assign(category, { name, description });
 
 	categories.push(category);
-
 	return response.status(201).send();
 });
 
