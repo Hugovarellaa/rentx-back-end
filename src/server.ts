db99e92dfc2012feb3e5725a3492a3e0f763e8bd
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-17 14:54:14 -0300

    Config server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { router } from "./routes";
 const PORT = 8080;
+
 const app = express();
+app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
