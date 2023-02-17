317b6c396fb8bd3999ad4129cebf64977bdd7b6b
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-17 14:35:13 -0300

    chore: create CategoryRepository

--- src/modules/cars/repositories/implementations/CategoryRepository.ts
+++ src/modules/cars/repositories/implementations/CategoryRepository.ts
@@ -1,3 +1,16 @@
-class CategoryRepository {}
+import { Category } from '../../entities/Categories';
+import { ICategoryRepository, ICreateCategoryDTO } from '../ICategoryRepository';
+
+class CategoryRepository implements ICategoryRepository {
+	create({ name, description }: ICreateCategoryDTO): void {
+		throw new Error('Method not implemented.');
+	}
+	findByName(name: string): Category {
+		throw new Error('Method not implemented.');
+	}
+	list(): Category[] {
+		throw new Error('Method not implemented.');
+	}
+}
 
 export { CategoryRepository };
