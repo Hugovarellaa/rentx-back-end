2b59fadb9bfd882b3d5b69dec3853ead59cc5f37
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-17 15:10:16 -0300

    Method Create categories

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,3 +1,24 @@
-class CategoriesRepository {}
+import { Category } from '../../entities/Category';
+import { ICategoriesRepository, ICreateCategoryDTO } from '../ICategoriesRepository';
+
+class CategoriesRepository implements ICategoriesRepository {
+	private categories: Category[];
+	constructor() {
+		this.categories = [];
+	}
+
+	create({ name, description }: ICreateCategoryDTO): void {
+		const category = new Category();
+		Object.assign(category, { name, description });
+
+		this.categories.push(category);
+	}
+	findByName(name: string): Category {
+		throw new Error('Method not implemented.');
+	}
+	list(): Category[] {
+		throw new Error('Method not implemented.');
+	}
+}
 
 export { CategoriesRepository };
