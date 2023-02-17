ce56cdbae10b231119e378d361bade679614f08c
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-17 17:43:06 -0300

    :tada: Stage 1 completed

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { router } from './routes';
+import swaggerDocument from './swagger.json';
 
 const PORT = 8080;
 
 const app = express();
 app.use(express.json());
 app.use(router);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
