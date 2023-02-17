71d461efa0c98cea25d07dc88313287879992c13
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-17 13:19:48 -0300

    Using router

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
-import express from "express";
+import express from 'express';
+
+import { router } from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
-app.listen(8080, () => console.log("listening on port 8080"));
+app.listen(8080, () => console.log('listening on port 8080'));
