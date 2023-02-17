a62b99d20be3b8a2aa6b9114269e8d9cd6f3f5f6
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-17 15:38:15 -0300

    Method post and Get in Specifications

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -10,6 +10,14 @@ specificationRoutes.post('/', (request, response) => {
 	const { name, description } = request.body;
 
 	const createSpecificationService = new CreateSpecificationService(specificationsRepository);
+	createSpecificationService.execute({ name, description });
+
+	return response.status(201).send(0);
+});
+
+specificationRoutes.get('/', (request, response) => {
+	const specifications = specificationsRepository.list();
+	return response.json(specifications);
 });
 
 export { specificationRoutes };
