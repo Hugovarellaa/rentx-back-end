fba030b9c2de9120a6149fba572bece43bf1abac
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-17 15:29:07 -0300

    Method create Specification

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,3 +1,21 @@
-class SpecificationsRepository {}
+import { Specification } from '../../entities/Specification';
+import { ICreateSpecificationDTO, ISpecificationsRepository } from '../ISpecificationsRepository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+	private specifications: Specification[];
+	constructor() {
+		this.specifications = [];
+	}
+
+	create({ name, description }: ICreateSpecificationDTO): void {
+		const specification = new Specification();
+		Object.assign(specification, { name, description });
+		this.specifications.push(specification);
+	}
+	findByName(name: string): Specification {}
+	list(): Specification[] {
+		throw new Error('Method not implemented.');
+	}
+}
 
 export { SpecificationsRepository };
