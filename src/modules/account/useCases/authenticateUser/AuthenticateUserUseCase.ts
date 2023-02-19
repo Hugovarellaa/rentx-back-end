989b7be663a4aead11294d3c41206ae8ab9c4489
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-19 11:27:07 -0300

    Generate Token if user is authenticated

---
+++ src/modules/account/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+	email: string;
+	password: string;
+}
+
+interface IResponse {
+	user: {
+		name: string;
+		email: string;
+	};
+	token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+	constructor(
+		@inject('UsersRepository')
+		private userRepository: IUsersRepository,
+	) {}
+	async execute({ email, password }: IRequest): Promise<IResponse> {
+		const user = await this.userRepository.findByEmail(email);
+
+		// Usuário existir
+		if (!user) {
+			throw new Error(`Email or password incorrect`);
+		}
+
+		// Senhas esta correta
+		const passwordMatch = await compare(password, user.password);
+		if (!passwordMatch) {
+			throw new Error(`Email or password incorrect`);
+		}
+
+		// Gerar o Web-token
+		const token = sign({}, 'supersecret123', {
+			subject: user.id,
+			expiresIn: '1d',
+		});
+
+		const tokenReturn: IResponse = {
+			user: {
+				name: user.name,
+				email: user.email,
+			},
+			token,
+		};
+
+		return tokenReturn;
+	}
+}
+
+export { AuthenticateUserUseCase };
