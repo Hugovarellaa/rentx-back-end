3442590402e467efcd6cb8c3ecfd98ca9d8242c9
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-24 16:39:54 -0300

    List categories - Get

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,13 @@
+import { Category } from '../../entities/Category';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+class ListCategoriesUseCase {
+	constructor(private categoriesRepository: ICategoriesRepository) {}
+	execute(): Category[] {
+		const categories = this.categoriesRepository.findAll();
+
+		return categories;
+	}
+}
+
+export { ListCategoriesUseCase };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,16 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/', createCategoryController.handle);
+categoriesRoutes.post('/', (request, response) => {
+	return createCategoryController.handle(request, response);
+});
 
 categoriesRoutes.get('/', (request, response) => {
-	const all = categoriesRepository.findAll();
-
-	return response.json(all);
+	return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
