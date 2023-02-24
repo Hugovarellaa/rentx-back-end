23c8d70ad0182ab694ddc39fbfa0969971dc479b
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-24 13:55:06 -0300

    Interface create category

--- src/repositories/ICategoriesRepository.ts
+++ src/repositories/ICategoriesRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateCategoryDTO } from '../dtos/ICreateCategory';
+import { Category } from '../entities/Category';
 
 interface ICategoriesRepository {
 	create({ name, description }: ICreateCategoryDTO): void;
+	findByName(name: string): Category;
 }
 
 export { ICategoriesRepository };
