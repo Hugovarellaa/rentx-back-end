bb96012da1ff66ea8081ad7ea6785ca23785b7c8
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-27 16:53:28 -0300

    bug

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -3,7 +3,7 @@ import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
 
 import { AppError } from '../../../../errors/AppError';
-import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
 	email: string;
@@ -12,8 +12,8 @@ interface IRequest {
 
 interface IResponse {
 	user: {
-		email: string;
 		name: string;
+		email: string;
 	};
 	token: string;
 }
@@ -22,33 +22,37 @@ interface IResponse {
 class AuthenticateUserUseCase {
 	constructor(
 		@inject('UsersRepository')
-		private usersRepository: UsersRepository,
+		private userRepository: IUsersRepository,
 	) {}
 	async execute({ email, password }: IRequest): Promise<IResponse> {
-		const user = await this.usersRepository.findByEmail(email);
+		const user = await this.userRepository.findByEmail(email);
+
+		// Usuário existir
 		if (!user) {
-			throw new AppError('Email or Password incorrect');
+			throw new AppError(`Email or password incorrect`);
 		}
 
-		const passwordMatch = compare(password, user.password);
+		// Senhas esta correta
+		const passwordMatch = await compare(password, user.password);
 		if (!passwordMatch) {
-			throw new AppError('Email or Password incorrect');
+			throw new AppError(`Email or password incorrect`);
 		}
 
+		// Gerar o Web-token
 		const token = sign({}, 'supersecret123', {
 			subject: user.id,
 			expiresIn: '1d',
 		});
 
-		const returnToken: IResponse = {
+		const tokenReturn: IResponse = {
 			user: {
-				email: user.email,
 				name: user.name,
+				email: user.email,
 			},
 			token,
 		};
 
-		return returnToken;
+		return tokenReturn;
 	}
 }
 
