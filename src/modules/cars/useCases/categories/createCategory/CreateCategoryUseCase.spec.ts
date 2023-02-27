49f062646a5ca347be3395692caa9852c9f67060
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-27 15:33:57 -0300

    create: Test - should not be able to create a new category with same name

--- src/modules/cars/useCases/categories/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/categories/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../../../errors/AppError';
 import { CategoriesRepositoryInMemory } from '../../../repositories/implementations/in-memory/CategoriesRepositoryInMemory';
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
@@ -10,7 +11,7 @@ describe('Create Category', () => {
 		createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
 	});
 
-	it('should be able to create a category', async () => {
+	it('should be able to create a new category', async () => {
 		const category = {
 			name: 'Category name Test',
 			description: 'Category description Test',
@@ -21,5 +22,18 @@ describe('Create Category', () => {
 		const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name);
 
 		expect(categoryCreated).toHaveProperty('id');
+	});
+
+	it('should not be able to create a new category with same name', async () => {
+		expect(async () => {
+			const category = {
+				name: 'Category name Test',
+				description: 'Category description Test',
+			};
+
+			await createCategoryUseCase.execute(category);
+
+			await createCategoryUseCase.execute(category);
+		}).rejects.toBeInstanceOf(AppError);
 	});
 });
