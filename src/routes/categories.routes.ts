65aec0522995e598863ef49346ebec9c7a6ecd29
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-27 18:59:53 -0300

    task: create method post categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 
+import { Category } from '../entities/Category';
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request, response) => {
 	const { name, description } = request.body;
-	const category = {
-		name,
-		description,
-	};
+
+	const category = new Category();
+	Object.assign(category, { name, description });
 
 	categories.push(category);
 
