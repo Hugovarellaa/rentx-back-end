0138781fca3cbdc5724e2f1a41c168ce5d483b9c
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-27 17:29:48 -0300

    Server started

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/a", (req, res) => {
+  return res.json({ message: "tudo funcionando 🚀🚀🚀" });
+});
+
+app.listen(8080, () => console.log("listening on port 8080"));
