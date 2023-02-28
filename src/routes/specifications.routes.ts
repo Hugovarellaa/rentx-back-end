07a3aaaaca7ab07f870fe0741960d9068be07cf9
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-27 21:27:37 -0300

    Task: create routes specifications

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+import { SpecificationRepository } from '../repositories/implementations/SpecificationRepository';
+
+const specificationsRoutes = Router();
+const specificationRepository = new SpecificationRepository();
+
+specificationsRoutes.post('/', (request, response) => {
+	const { name, description } = request.body;
+
+	const specificationsAlreadyExists = specificationRepository.findByName(name);
+	if (specificationsAlreadyExists) {
+		throw new Error(`Category ${name} already exists`);
+	}
+
+	specificationRepository.create({ name, description });
+
+	return response.status(201).send();
+});
+
+specificationsRoutes.get('/', (request, response) => {
+	const all = specificationRepository.findAll();
+	return response.status(200).json(all);
+});
+
+export { specificationsRoutes };
