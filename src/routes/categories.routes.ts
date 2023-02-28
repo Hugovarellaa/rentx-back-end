42f18a6217a640912bc0234f477ed7fbb6c9f0f1
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-27 21:49:42 -0300

    Task: creating and refactoring routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,16 @@
 import { Router } from 'express';
 
-import { CategoryRepository } from '../modules/cars/repositories/implementations/CategoryRepository';
 import { createCategoriesController } from '../modules/cars/useCases/Categories/createCategories';
+import { listCategoriesController } from '../modules/cars/useCases/Categories/listCategoires';
 
 const categoriesRoutes = Router();
-const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post('/', (request, response) => {
 	return createCategoriesController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-	const all = categoryRepository.findAll();
-	return response.status(200).json(all);
+	return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
