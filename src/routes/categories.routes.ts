4a7b6c98853c50ab351f2100ae9c1821077040ae
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-27 21:40:46 -0300

    Task: Refactoring categoriesRoutes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,13 @@
 import { Router } from 'express';
 
 import { CategoryRepository } from '../modules/cars/repositories/implementations/CategoryRepository';
+import { createCategoriesController } from '../modules/cars/useCases/Categories/createCategories';
 
 const categoriesRoutes = Router();
 const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post('/', (request, response) => {
-	const { name, description } = request.body;
-
-	const categoriesAlreadyExists = categoryRepository.findByName(name);
-	if (categoriesAlreadyExists) {
-		throw new Error(`Category ${name} already exists`);
-	}
-
-	categoryRepository.create({ name, description });
-
-	return response.status(201).send();
+	return createCategoriesController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
