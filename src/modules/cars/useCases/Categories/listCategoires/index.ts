735b534a786a8e055c584360014a674dab721a9c
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-27 21:49:20 -0300

    Task: import all

---
+++ src/modules/cars/useCases/Categories/listCategoires/index.ts
@@ -0,0 +1,9 @@
+import { CategoryRepository } from '../../../repositories/implementations/CategoryRepository';
+import { ListCategoriesController } from './ListCategoriesController';
+import { ListCategoriesUseCase } from './ListCategoriesUseCase';
+
+const categoryRepository = new CategoryRepository();
+const listCategoriesUseCase = new ListCategoriesUseCase(categoryRepository);
+const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
+
+export { listCategoriesController };
