240bd65d3d53e4739122be38bfc73fdff2094b34
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-01 18:55:19 -0300

    Remove: manualy Singleton pattern to automatic

--- src/modules/cars/useCases/Categories/importCategories/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoryRepository } from '../../../repositories/implementations/CategoryRepository';
-import { ImportCategoriesController } from './ImportCategoriesController';
-import { ImportCategoriesUseCase } from './ImportCategoriesUseCase';
-
-const categoryRepository = CategoryRepository.getInstance();
-const importCategoriesUseCase = new ImportCategoriesUseCase(categoryRepository);
-const importCategoriesController = new ImportCategoriesController(importCategoriesUseCase);
-
-export { importCategoriesController };
