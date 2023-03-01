d6f42d06b06d739c06d172063be06befe3fc4c5d
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-01 18:52:03 -0300

    Task: Repository categories in memory

---
+++ src/modules/cars/repositories/implementations/in-memory/CategoryRepositoryInMemory.ts
@@ -0,0 +1,29 @@
+import { ICreateCategoriesDTO } from '../../../dtos/CreateCategoriesDTO';
+import { Category } from '../../../entities/Category';
+import { ICategoryRepository } from '../../ICategoryRepository';
+
+class CategoryRepositoryInMemory implements ICategoryRepository {
+	private categories: Category[];
+
+	private constructor() {
+		this.categories = [];
+	}
+
+	create({ name, description }: ICreateCategoriesDTO): void {
+		const category = new Category();
+		Object.assign(category, { name, description });
+
+		this.categories.push(category);
+	}
+
+	findAll(): Category[] {
+		return this.categories;
+	}
+
+	findByName(name: string): Category {
+		const categoriesAlreadyExists = this.categories.find((c) => c.name === name);
+		return categoriesAlreadyExists;
+	}
+}
+
+export { CategoryRepositoryInMemory };
