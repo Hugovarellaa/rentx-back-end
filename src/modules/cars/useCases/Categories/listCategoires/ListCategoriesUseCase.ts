3aedc59c6b85a859d31b290374464d1911618fc0
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-01 19:21:13 -0300

    dependency injection with tsyringe

--- src/modules/cars/useCases/Categories/listCategoires/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/Categories/listCategoires/ListCategoriesUseCase.ts
@@ -1,10 +1,18 @@
+import { inject, injectable } from 'tsyringe';
+
 import { Category } from '../../../entities/Category';
 import { CategoryRepository } from '../../../repositories/implementations/CategoryRepository';
 
+@injectable()
 class ListCategoriesUseCase {
-	constructor(private categoryRepository: CategoryRepository) {}
-	execute(): Category[] {
-		return this.categoryRepository.findAll();
+	constructor(
+		@inject('CategoryRepository')
+		private categoryRepository: CategoryRepository,
+	) {}
+	async execute(): Promise<Category[]> {
+		const categories = await this.categoryRepository.findAll();
+
+		return categories;
 	}
 }
 
