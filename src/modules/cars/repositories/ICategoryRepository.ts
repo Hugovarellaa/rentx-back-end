18690e6c086fab4efdf7d32607409087e9e2f263
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-01 18:53:50 -0300

    Task: Add async

--- src/modules/cars/repositories/ICategoryRepository.ts
+++ src/modules/cars/repositories/ICategoryRepository.ts
@@ -2,9 +2,9 @@ import { ICreateCategoriesDTO } from '../dtos/CreateCategoriesDTO';
 import { Category } from '../entities/Category';
 
 interface ICategoryRepository {
-	create({ name, description }: ICreateCategoriesDTO): void;
-	findAll(): Category[];
-	findByName(name: string): Category;
+	create({ name, description }: ICreateCategoriesDTO): Promise<void>;
+	findAll(): Promise<Category[]>;
+	findByName(name: string): Promise<Category>;
 }
 
 export { ICategoryRepository };
