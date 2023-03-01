2b1f073569b66ae18c9e5ee32d41bb8e7e818f8d
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-01 18:53:57 -0300

    Task: Add async

--- src/modules/cars/repositories/ISpecificationRepository.ts
+++ src/modules/cars/repositories/ISpecificationRepository.ts
@@ -2,9 +2,9 @@ import { ICreateSpecificationsDTO } from '../dtos/CreateSpecificationsDTO';
 import { Specification } from '../entities/Specification';
 
 interface ISpecificationRepository {
-	create({ name, description }: ICreateSpecificationsDTO): void;
-	findAll(): Specification[];
-	findByName(name: string): Specification;
+	create({ name, description }: ICreateSpecificationsDTO): Promise<void>;
+	findAll(): Promise<Specification[]>;
+	findByName(name: string): Promise<Specification>;
 }
 
 export { ISpecificationRepository };
