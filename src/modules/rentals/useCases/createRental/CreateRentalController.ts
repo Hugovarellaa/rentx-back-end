d3baa7da484f8f5ae54444ebffd51056cf18cfb0
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-11 17:32:47 -0300

    fix: Stop project before create table rentals

--- src/modules/rentals/useCases/createRental/CreateRentalController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-class CreateRentalController {
-	async handle(request: Request, response: Response) {
-		console.log('ok');
-	}
-}
-
-export { CreateRentalController };
--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++
@@ -1,7 +0,0 @@
-interface IDateProvider {
-	convertToUtc(date: Date): string;
-	compareInHours(start_date: Date, end_date: Date): number;
-	dateNow(): Date;
-}
-
-export { IDateProvider };
